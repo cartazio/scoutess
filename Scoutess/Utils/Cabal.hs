39b81064734f62b426b23ca76b33869b75abaa64
Jeremy Shaw <jeremy@seereason.com>
2012-03-08 06:24:13 +0000

    started work on a pipes-based wrapper around cabal-install after looking in the cabal-install source for a while.. it seems like calling the cabal install executable is our only real choice. The functionality we need is to exported -- at it is rather complicated to recreate. Seems better to just call the executable 'cabal' for now.

---
+++ Scoutess/Utils/Cabal.hs
@@ -0,0 +1,26 @@
+module Scoutess.Util.Cabal where
+
+import Control.Pipe
+import Control.Pipe.Process
+import Data.ByteString
+import Data.Void
+import System.Exit
+
+cabal :: FilePath -- ^ path to directory that contains the @.cabal@
+      -> [String] -- ^ arguments to pass to 'cabal'
+      -> Pipe Void (Either ByteString ByteString) IO ExitCode
+cabal workingDirectory args =
+    process "cabal" args (Just workingDirectory) Nothing
+
+configure :: FilePath -- ^ path to directory that contains the @.cabal@
+          -> [String] -- ^ flags to pass to @cabal configure@
+          -> Pipe Void (Either ByteString ByteString) IO ExitCode
+configure workingDirectory args =
+    cabal workingDirectory  ("configure":args)
+
+haddock :: FilePath -- ^ path to directory that contains the @.cabal@
+        -> [String] -- ^ flags to pass to @cabal haddock@
+        -> Pipe Void (Either ByteString ByteString) IO ExitCode
+haddock workingDirectory args =
+    cabal workingDirectory  ("haddock":args)
+
