63ef3c7ae14354cca46cf4cf7c7d32c3310d3352
Jeremy Shaw <jeremy@seereason.com>
2012-05-21 23:10:56 +0000

    Main.hs: added reform, updated some other packages

--- Main.hs
+++ Main.hs
@@ -13,10 +13,10 @@ main :: IO ()
 main =
     let packages =
             [ "acid-state-0.6.3"
-            , "happstack-server-7.0.1"
+            , "happstack-server-7.0.2"
             , "happstack-server-tls-7.0.0"
             , "happstack-hsp-7.1.0"
-            , "hsx-jmacro-7.1.0"
+            , "hsx-jmacro-7.1.1"
             , "happstack-jmacro-7.0.1"
             , "happstack-heist-7.0.0"
             , "happstack-7.0.0"
@@ -24,7 +24,12 @@ main =
             , "happstack-lite-7.2.0"
             , "happstack-hstringtemplate-7.0.1"
             , "happstack-clientsession-7.1.0"
+--            , "happstack-yui-7351.2.0"
             -- , "happstack-plugins-7.0.1")
+            , "reform-0.1.1"
+            , "reform-blaze-0.1"
+            , "reform-happstack-0.1.1"
+            , "reform-hsp-0.1.1"
             , "ixset-1.0.3"
             , "web-routes-0.27.1"
             , "web-routes-boomerang-0.26.0"
