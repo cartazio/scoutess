2e5c97ba28217a91fcc2443fd65ce7c704635085
dmcgill9071 <dmcgill9071@gmail.com>
2012-06-17 23:54:25 +0000

    implemented DataFlow.calculateDependencies

--- Scoutess/DataFlow.hs
+++ Scoutess/DataFlow.hs
@@ -3,17 +3,25 @@ module Scoutess.DataFlow where
 
 import Control.Arrow
 import Control.Category
-import Control.Monad
+import Control.Monad (liftM)
+import Control.Monad.State (State, runState, get, put, execState)
+import Data.Array (Array, array)
+import Data.Bimap (Bimap)-- O(log n) bijection between 'Int's and 'VersionInfo's used in 'calculateDependencies'
+import qualified Data.Bimap as B
+import Data.Graph (Graph, Vertex)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Data.Text (Text)
 import Distribution.Package
 import Prelude hiding ((.), id)
 import qualified Prelude
+
 import qualified Scoutess.Service.Source.Hackage as H (fetchAllVersions)
 
 newtype Scoutess a b = Scoutess (Kleisli IO a b)
     deriving (Category, Arrow, ArrowApply, ArrowChoice, ArrowLoop, ArrowPlus, ArrowZero)
+
+liftScoutess :: (a -> IO b) -> Scoutess a b
 liftScoutess = Scoutess . Kleisli
 
 data SourceSpec
@@ -45,7 +53,7 @@ data VersionSpec = VersionSpec
 data VersionInfo = VersionInfo
     {
     }
-    deriving Show
+    deriving (Show, Eq, Ord)
 
 data PriorRun = PriorRun
     {
@@ -58,9 +66,9 @@ data BuildSpec = BuildSpec
     deriving Show
 
 data DependencyGraph = DependencyGraph
-    {
+    { graph :: Graph
+    , association :: Bimap Vertex VersionInfo
     }
-    deriving Show
 
 data LocalHackageIndex = LocalHackageIndex
     {
@@ -82,12 +90,12 @@ standard sourceFilter versionFilter =
 
 fetchVersions :: Scoutess SourceSpec VersionSpec
 fetchVersions = liftScoutess $ \sourceSpec -> do
-    let locations' = S.toList . locations $ sourceSpec
-    versions <- liftM unions (mapM fetchVersionsFrom locations')
-    return (VersionSpec{versionInfos = versions})
+    let locations' = S.toList (locations sourceSpec)
+    versionsL <- mapM fetchVersionsFrom locations'
+    return VersionSpec{versions = S.unions versionsL}
 
 fetchVersionsFrom :: SourceLocation -> IO (Set VersionInfo)
-fetchVersionsFrom Hackage = toVersionInfos H.fetchAllVersions sourceConfig
+fetchVersionsFrom Hackage = toVersionInfos `liftM` H.fetchAllVersions sourceConfig
     where toVersionInfos :: Set (Text,Text) -> Set VersionInfo
           -- ^ possibly defined in Scoutess.Service.Source.Hackage?
           toVersionInfos  = undefined
@@ -95,9 +103,51 @@ fetchVersionsFrom Hackage = toVersionInfos H.fetchAllVersions sourceConfig
 fetchVersionsFrom  _      = undefined
 
 calculateDependencies :: Scoutess (TargetSpec, VersionSpec) DependencyGraph
-calculateDependencies = undefined
+calculateDependencies = liftScoutess $ \(targetSpec, versionSpec) ->
+    let targetVersion :: VersionInfo
+        targetVersion = undefined -- convert targetSpec into a VersionInfo
 
+        depMap :: Set (VersionInfo, (Set VersionInfo))
+        depMap = dependencyMap versionSpec targetVersion
 
+        -- change the container types and transform all of the 'VersionInfo's
+        -- into the corresponding 'Vertex'
+        toVersionInfos (v,deps) = do
+            v'    <- getVersionIndex v
+            deps' <- mapM getVersionIndex (S.toList deps)
+            return (v',deps')
+        (vertexList, bimap) = runState (mapM toVersionInfos (S.toList depMap)) B.empty
+        bounds = (0, B.size bimap -1)
+        depArr :: Array Vertex [Vertex]
+        depArr = array bounds vertexList
+    in return DependencyGraph {graph = depArr, association = bimap}
+
+-- | Return the immediate dependencies of a given 'VersionInfo'
+getImmDeps :: VersionSpec -> VersionInfo -> Set VersionInfo
+getImmDeps  = undefined
+
+-- | Find the index of a VersionInfo (adding it to the 'Bimap' if it isn't found).
+getVersionIndex :: VersionInfo -> State (Bimap Int VersionInfo) Int
+getVersionIndex version = do
+    bimap <- get
+    let addNew = do
+        let ix = B.size bimap
+        put $ B.insert ix version bimap
+        return ix
+    maybe addNew return (B.lookupR version bimap)
+
+-- | create a 'Set' consisting of all the required 'VersionInfo's and each of their dependencies
+dependencyMap :: VersionSpec -> VersionInfo -> Set (VersionInfo, (Set VersionInfo))
+dependencyMap spec version = execState (dependencyMap' spec version) S.empty
+    where
+    dependencyMap' :: VersionSpec -> VersionInfo -> State (Set (VersionInfo, (Set VersionInfo))) ()
+    dependencyMap' spec version = do
+        depMap <- get
+        let deps    = getImmDeps spec version
+            newDeps = deps S.\\ (S.map fst depMap) -- get rid of dependencies we've already seen
+            depMap' = S.insert (version, newDeps) depMap
+        put depMap'
+        mapM_ (dependencyMap' spec) (S.toList newDeps)
 
 calculateChanges :: Scoutess (TargetSpec, PriorRun, DependencyGraph) BuildSpec
 calculateChanges = undefined
